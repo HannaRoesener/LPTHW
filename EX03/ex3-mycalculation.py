6888776b2e8765aa72950e110f2cd936d3a8b748
Hanna Rösener <Hanna@Hannas-MacBook-Pro.local>
2016-10-25 12:58:12 +0200

    Session0 ex1-14 & new files

---
+++ EX03/ex3-mycalculation.py
@@ -0,0 +1,10 @@
+print "For how many hours a week do I have classes at university (apprx)?"
+
+print "Monday", 0
+print "Tuesday", 1.5 * 2 + 1.5
+print "Wednesday", 1.5 * 2
+print "Thursday", (6.75 + 3.5) / 2
+print "Friday", 3.5 / 2
+print "Weekends occasionally"
+
+print "All days added up:", (1.5 * 2 + 1.5) + (1.5 * 2) + ((6.75 + 3.5) / 2) + (3.5 / 2)
---
+++ EX03/ex3.py
@@ -0,0 +1,30 @@
+# print line
+print "I will count my chickens:"
+
+# calculates number of hens
+print "Hens", 25 + 30 / 6
+# calculates number of roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints line
+print "Now I will count the eggs:"
+
+# calculates equation 
+print 3 + 2 + 2 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# answers the question 
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh that's why it's False."
+
+print "How about some more."
+
+# prints first part and calculates equation after comma
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
