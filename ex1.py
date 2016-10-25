5d653b589da98fbd15688adf8a7b6122ae440b4f
HannaRoesener <hanna.roesener@stud.leuphana.de>
2016-10-25 12:46:39 +0200

    Delete ex1.py

--- ex1.py
+++
@@ -1,8 +0,0 @@
-print "Hello World!"
-# print "Hello Again"
-print "I like typing this."
-print "This is fun."
-#'Yay! Printing.'
-#print "I'd much rather you 'not'."
-'I "said" do not touch this.'
-'Do you even listen to me?'
